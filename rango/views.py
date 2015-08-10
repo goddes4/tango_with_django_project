ed808f13b820193a9a3117411ef6e51f21ca2991
tykim <goddes4@gmail.com>
2015-08-10 20:55:21 +0900

    * Creating a Django Project * Creating a Django Application * Creating a view * Mapping URLs

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,10 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse(" Rango says: Hello world! <br/> <a href='/rango/about'>About</a>")
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
